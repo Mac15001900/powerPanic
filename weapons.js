0ab18c898b812ad82e688a1d139eae2c64b696c4
auhbon <auhbon@gmail.com>
2020-10-24 15:26:49 -0700

    All pieces spawn when/where we expect them to do: add collisions between the pieces themeselves

--- weapons.js
+++ weapons.js
@@ -16,16 +16,16 @@ var SceneWeapons = new Phaser.Class({
         console.log('Preload in weapons');
         this.load.image('weapons-icon', 'assets/icon-laser.png');   
         this.load.image('missile-top', 'assets/missile-top.png');   
-        this.load.image('missile-middle1', 'assets/missile-middle1.png');   
-        this.load.image('missile-middle2', 'assets/missile-middle2.png');   
+        this.load.image('missile-mid1', 'assets/missile-middle1.png');   
+        this.load.image('missile-mid2', 'assets/missile-middle2.png');   
         this.load.image('missile-bottom', 'assets/missile-bottom.png');   
     },
 
     create: function () {
         this.isPlaying = false;
-        var g1 = this.add.grid(350, 350, 400, 400, 16, 16, 0x057605);
+        var g1 = this.add.grid(350, 399, 400, 400, 16, 16, 0x057605);
 
-        const rect = this.add.rectangle(350, 550, 400, 10, 0x1EE530, 1);
+        // const floor = this.add.rectangle(350, 550, 400, 10, 0x1EE530, 1);
 
         var text = this.add.text(200, 100, '', { font: "32px Arial", fill: "#19de65" });
         text.text = 'You are in weapons';
@@ -39,15 +39,24 @@ var SceneWeapons = new Phaser.Class({
         // var floor;
         // floor = new Phaser.Rectangle(20,20,20,20);
         
-        this.bottom;
-        this.mid1;
-        this.mid2;
-        this.top;
+        var currentPiece;
+        this.bottom = {
+            name: 'missile-bottom'
+        };
+        this.mid1 = {
+            name: 'missile-mid1'
+        };
+        this.mid2 = {
+            name: 'missile-mid2'
+        };
+        this.top = {
+            name: 'missile-top'
+        };
 
 
-        // this.physics.add.collider(this.bottom, g1);
+        // this.physics.add.collider(this.bottom, floor);
 
-        this.initializeMissileGame();
+        this.initializeMissileGame(this.bottom);
         // this.bottom = this.add.image(700,500,'missile-bottom');
         // this.mid1 = this.add.image(700,400,'missile-middle1');
         // this.mid2 = this.add.image(700,300,'missile-middle2');
@@ -55,45 +64,103 @@ var SceneWeapons = new Phaser.Class({
     },
 
     update: function (timestep, dt) {
-        if(this.backKey.isDown){
+        // if(gameStatus !== GS.GAME_STARTED) {
+
+        // }
+        // console.log(this.currentPiece.texture.key);
+        if(this.backKey.isDown) {
             console.log('Switching back to menu');
             this.scene.start('SceneStart');
         }
 
-        if(this.spaceKey.isDown)
+        if(Phaser.Input.Keyboard.JustDown(this.spaceKey))
             this.dropPiece();
 
     },
 
-    initializeMissileGame: function() {
+    initializeMissileGame: function(piece) {
         this.isPlaying = true;
-        this.currentPiece = this.bottom;
-        this.startPiece();
+        // this.currentPiece = this.bottom;
+        this.startPiece(piece);
     },
     
-    startPiece: function () {
-        var x, y, i, v;
-        if (this.currentPiece == this.bottom) {
+    startPiece: function (piece) {
+        console.log("In start piece function");
+        let x, y, i, v;
+        if (piece.name === this.bottom.name) {
+            console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
             x = 700;
             y = 500;
             i = 'missile-bottom';
             v = -200;
+        } else if (piece.name === this.mid1.name) {
+            console.log("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
+            x = 700;
+            y = 400;
+            i = 'missile-mid1';
+            v = -200;
+        } else if (piece.name === this.mid2.name) {
+            console.log("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
+            x = 700;
+            y = 300;
+            i = 'missile-mid2';
+            v = -200;
+        } else if (piece.name === this.top.name) {
+            console.log("PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP");
+            x = 700;
+            y = 200;
+            i = 'missile-top';
+            v = -200;
         }
-        
-        this.currentPiece = this.physics.add.image(x,y,i);
-        this.currentPiece.setVelocity(v, 0);
-        this.currentPiece.setBounce(1, 0);
-        this.currentPiece.setCollideWorldBounds(true);
 
+        piece = this.physics.add.image(x,y,i);
+        piece.setVelocity(v, 0);
+        piece.setBounce(1, 0);
+        piece.setCollideWorldBounds(true);
+
+        this.currentPiece = piece;
         // emitter.startFollow(this.currentPiece);
     },
 
-    dropPiece: function () {
-        console.log("We made it into the drop funk! YEEHAW");
-        this.currentPiece.setVelocity(0,.3);
+    dropPiece: function () {        
+        // console.log("We made it into the drop funk! YEEHAW");
+        console.log("In drop piece function"); 
+        
+        // console.log("this.currentPiece is:");
+        // console.log(this.currentPiece); 
+        
+        // console.log("this.currentPiece.name is:");
+        // console.log(this.currentPiece.name); 
+
+        console.log("this.currentPiece.texture.key is:");
+        console.log(this.currentPiece.texture.key); 
+        
+        this.currentPiece.setVelocity(0,100);
+
+        if (this.currentPiece.texture.key === this.mid2.name) {
+            this.startPiece(this.top);
+        } else if (this.currentPiece.texture.key === this.mid1.name) {
+            this.startPiece(this.mid2);
+        } else if (this.currentPiece.texture.key === this.bottom.name) {
+            this.startPiece(this.mid1);            
+        }
+
+        // if (this.currentPiece.name === this.mid2.name) {
+        //     this.startPiece(this.top);
+        // } else if (this.currentPiece.name === this.mid1.name) {
+        //     this.startPiece(this.mid2);
+        // } else if (this.currentPiece.name === this.bottom.name) {
+        //     this.startPiece(this.mid1);            
+        // }
+
+        // this.startPiece(this.currentPiece);
     },
 
     receiveMessage: function (data) {
         console.log(data);
-    },        
+        //when receive message like "Start game"
+    },
 });
+
+
+// tearsOfPainAndExhaustion.mp4
