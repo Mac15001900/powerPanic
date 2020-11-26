60aced44d19b4f1ec7783f1d87016588ee9006a2
Mac15001900 <mac15001900@gmail.com>
2020-11-27 00:21:33 +0100

    Added instructions to comms

--- comms.js
+++ comms.js
@@ -19,7 +19,22 @@ var SceneComms = new Phaser.Class({
 
     create: function () {
 
-        this.womp = this.add.text(50,100, 'Comms needs your help! \nTranslate the messages correctly to clear the civilian ships. \n Hover over each character and find the green characters to spell the message', { font: "20px Arial", fill: "#19de65" });
+        //Instructions
+        this.instructionsBackground = this.add.graphics();
+        this.instructionsBackground.depth = 9001;
+        this.instructionsBackground.fillStyle(0x000000, 1);
+        this.instructionsBackground.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+        this.instructionsText = this.add.text(20, 64, '', { font: "16px Arial", fill: "#19de65", wordWrap:{width:CANVAS_WIDTH-40} });
+        this.instructionsText.text = "You are the comms specialist.\n\n" +
+            "Your job is to send messages to civilian ships tellling them to stay away. Unofrtunely, the translator system is somewhat broken now, and will need some help.\n\n"+
+            "Mouse over letters to see which of them are part of the message - those that are will light up green. Click on all of them and send a message make all civian ships fly away.\n\n"+
+            "If you transcribe the message incorrectly ships might be confused, and will come to check if you're ok.\n\n" +
+            "Broadcasting messages uses up power. If you're close to an overload, you might need to send empty ones.\n\n" +
+          "\nWaiting for the pilot to start the game...";
+        this.instructionsText.depth = 9002;
+
+        //this.womp = this.add.text(50,100, 'Comms needs your help! \nTranslate the messages correctly to clear the civilian ships. \n Hover over each character and find the green characters to spell the message', { font: "20px Arial", fill: "#19de65" });
         this.pomp = this.add.text(50,200, '', { font: "20px Arial", fill: "#19de65" });
 
         var powerIcon = this.add.image(96,48,'power-icon');
@@ -28,22 +43,22 @@ var SceneComms = new Phaser.Class({
 
 
         this.passwordList = [
-            "CLEAR", "EVACUATE", "GOAWAY", "RUNAWAY", "LEAVE", "CLEARTHEAREA", "FLEE", "SCARPER", "SCRAM", "RUNFOOLS", "RUNYOUFOOLS", "THISISBAD", 
-            "DONTCOMEHERE", "SERIOUSLYRUN", "BIGEXPLOSION", "EXPLOSIONS", "EXPLODE", "BOOMBOOM", "KABOOM", "KABOOOOM", "DANGER", "PANIC", "DOPANIC",
-            "DEATH", "DESTRUCTION", "OVERLOAD", "SUPERNOVA", "MISSILES", "CONFUSION", "OVERPOWERED", "DONOTHELP", "REDALERT", "NOTSAFE", "CRISIS", 
-            "EMERGENCY", "THREAT", "TROUBLE", "HISSSS", "MENACE", "PRECARIOUS", "NOGOZONE", "BADLOCATION", "YOUMIGHTDIE",  "YOUWILLDIE",
+            "CLEAR", "EVACUATE", "GO_AWAY", "RUN_AWAY", "LEAVE", "CLEAR_THE_AREA", "FLEE", "SCARPER", "SCRAM", "RUN_FOOLS", "RUN_YOU_FOOLS", "THIS_IS_BAD", 
+            "DONT_COME_HERE", "SERIOUSLY_RUN", "BIGE_XPLOSION", "EXPLOSIONS", "EXPLODE", "BOOM_BOOM", "KABOOM", "KABOOOOM", "DANGER", "PANIC", "DO_PANIC",
+            "DEATH", "DESTRUCTION", "OVERLOAD", "SUPERNOVA", "MISSILES", "CONFUSION", "OVERPOWERED", "DO_NOT_HELP", "RED_ALERT", "NOT_SAFE", "CRISIS", 
+            "EMERGENCY", "THREAT", "TROUBLE", "HISSSS", "MENACE", "PRECARIOUS", "NO_GO_ZONE", "BAD_LOCATION", "YOU_MIGHT_DIE",  "YOU_WILL_DIE",
             ];
         this.password = this.passwordList[Math.floor(Math.random()*this.passwordList.length)];
         console.log(this.password);
         this.passwordPos = [];
-for (let i = 0; i < this.password.length; i++) {
-    let pos = Math.floor(Math.random()*64);
-    while(this.passwordPos.includes(pos)){
-        pos = Math.floor(Math.random()*64)
-    }
-    this.passwordPos.push(pos)
-}
-this.passwordPos.sort(function(a,b){return a-b});
+        for (let i = 0; i < this.password.length; i++) {
+            let pos = Math.floor(Math.random()*64);
+            while(this.passwordPos.includes(pos)){
+                pos = Math.floor(Math.random()*64)
+            }
+            this.passwordPos.push(pos)
+        }
+        this.passwordPos.sort(function(a,b){return a-b});
         this.squares = [];
         this.text = [];
         let counter = 0;
@@ -52,7 +67,7 @@ this.passwordPos.sort(function(a,b){return a-b});
             let y = 245+45*Math.floor(i/8);
             this.squares[i] = this.add.image(x,y,'square');
             this.text[i] = this.add.text(x,y, '', { font: "35px Arial", fill: "#19de65" }).setOrigin(0.5);
-            let letter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[Math.floor(Math.random()*26)]
+            let letter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ_'[Math.floor(Math.random()*27)]
             if(this.passwordPos.includes(i)){
                 letter = this.password[counter];
                 this.text[i].text = this.password[counter];
@@ -163,6 +178,9 @@ this.passwordPos.sort(function(a,b){return a-b});
             switchToScene(this,'SceneStart');
         }
         if(gameStatus !== GS.GAME_STARTED && !DEBUG_IGNORE_GAME_STATE) return;
+
+        this.instructionsBackground.setVisible(false);
+        this.instructionsText.setVisible(false);
 
         power += 2*dt/1000;
         this.powerBar.clear();
