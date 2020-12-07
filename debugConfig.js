bc626d15a608a7cc97d1d2fed8058a3bc63bec7f
Mac15001900 <mac15001900@gmail.com>
2020-12-07 20:31:40 +0100

    Labeled debugConfig options

--- debugConfig.js
+++ debugConfig.js
@@ -1,12 +1,12 @@
 var tempDebugConfigUntilIFigureOutHowToSynchronouslyLoadJSON = {
 	"allow_debug" : true, //Setting this to false will disable all other debug options
-	"ignore_taken_stations" : false,
-	"show_hitboxes" : false,
-	"use_dev_server" : false,
-	"use_random_server" : false,
-	"randomise_username" : false,
-	"ignore_game_state" : false,
-	"immortal" : false,
-	"pilot_packet_sending" : false,
-	"disable_messages" : false
+	"ignore_taken_stations" : false, //Allows for joining stations even if they're taken
+	"show_hitboxes" : false, //Shows hitboxes and velocities on all game objects
+	"use_dev_server" : false, //Skips room prompt, uses sever 'dev'
+	"use_random_server" : false, //Skips room promt, uses a server with a random name
+	"randomise_username" : false, //Skip username prompt, uses a random username
+	"ignore_game_state" : false, //Ignores the gameState, starts games immidiately after joining
+	"immortal" : false, //Disables dying (currently only for piloting)
+	"pilot_packet_sending" : false, //The pilot can send other stations' packets to simulate their effect
+	"disable_messages" : false //Disables sending network messages
 }
